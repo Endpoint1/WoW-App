06cbf65adeb133ff382089420660c95af6814a9c
amorrison61689 <30812407+amorrison61689@users.noreply.github.com>
2017-09-09 15:55:47 -0400

    Initial Panda

---
+++ app/src/main/java/com/amorr/wowapp/WoWApp.java
@@ -0,0 +1,13 @@
+package com.amorr.wowapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WoWApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wo_wapp);
+    }
+}
